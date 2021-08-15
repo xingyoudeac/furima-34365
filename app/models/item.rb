cca9620a904d9dc5d6c8075f9ff9d6e3f4454326
xingyoudeac <yukichishichuan@gmail.com>
2021-08-15 19:46:19 +0900

    出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,10 +23,12 @@ class Item < ApplicationRecord
   
   validates :price           ,presence: true  ,numericality:  { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }  ,format: { with: /\A[0-9]+\z/ }
 
-  validates :category_id     ,numericality:  { other_than: 1, message: "can't be blank" } 
-  validates :condition_id    ,numericality:  { other_than: 1, message: "can't be blank" }
-  validates :burden_id       ,numericality:  { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id   ,numericality:  { other_than: 1, message: "can't be blank" }
-  validates :day_id          ,numericality:  { other_than: 1, message: "can't be blank" }
-
+  with_options numericality:  { other_than: 1, message: "can't be blank" } do
+    validates :category_id      
+    validates :condition_id    
+    validates :burden_id       
+    validates :prefecture_id   
+    validates :day_id
+  end
+  
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,8 +29,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it "カテゴリーのカラムはidが1では投稿出来ない" do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it "商品の状態が無いと投稿出来ない" do
         @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "コンディションのカラムはidが1では投稿出来ない" do
+        @item.condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
@@ -39,13 +49,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden can't be blank")
       end
+      it "配送料の負担のカラムはidが1では投稿出来ない" do
+        @item.burden_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
       it "発送元の地域が無いと投稿出来ない" do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it "発送元の地域のカラムはidが1では投稿出来ない" do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
       it "発送までの日数がないと投稿出来ない" do
         @item.day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+      it "発送までの日数のカラムはidが1では投稿出来ない" do
+        @item.day_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Day can't be blank")
       end
@@ -55,17 +80,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "販売価格が¥299だと投稿出来ない" do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "販売価格が¥10,000,000だと投稿出来ない" do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it "販売価格は全角数字混合では投稿出来ない" do
         @item.price = '４000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "半角英数混合では投稿出来ない" do
+        @item.price = '4000q'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "半角英語だけでは投稿出来ない" do
+        @item.price = 'abcd'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
