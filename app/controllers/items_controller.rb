89a717e933ead279be0036cf26b573baa6aef185
xingyoudeac <yukichishichuan@gmail.com>
2021-08-19 16:59:53 +0900

    商品詳細機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
   
   def index
     @item = Item.all.order('created_at DESC')
@@ -18,10 +19,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :title, :concept, :category_id, :condition_id, :burden_id, :prefecture_id, :day_id, :day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :concept, :category_id, :condition_id, :burden_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
   
 end
