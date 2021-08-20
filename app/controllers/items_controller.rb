a133c4a1642b5bc25993fa7ff075ebdb6d4dc969
xingyoudeac <yukichishichuan@gmail.com>
2021-08-20 23:04:38 +0900

    情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :ser_item, only: [:show, :edit, :update]
   
   def index
     @item = Item.all.order('created_at DESC')
@@ -20,21 +20,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
     end
+  end
+
+  def ser_item
+    @item = Item.find(params[:id])
   end
 
 
